f75b9d1f1a1df750090d31888af4d61e52262877
Andrew Weeks <Weeksy@Andrews-MacBook-Pro.local>
2015-07-01 11:05:59 +0200

    second commit

--- UITableViewController Challenge Solution/TestTVC.h
+++ UITableViewController Challenge Solution/TestTVC.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  TestTVC.h
 //  UITableViewController Challenge Solution
 //
-//  Created by Andrew Weeks on 30/06/2015.
+//  Created by Andrew Weeks on 01/07/2015.
 //  Copyright (c) 2015 BlueBinProductions. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface TestTVC : UITableViewController
 
 @end
-
